ce82ddb215e14dfbc3e5943835b34bfccb6dc7d1
steve-ababio <stevo12424@live.com>
2024-04-12 16:26:49 +0000

    modified custom not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function NotFound(){
     return(
-        <main>
-            <h2>There was a problem</h2>
-            <p>We could not find the page you were looking for</p>
-            <p>Go back to the <Link href="/">Login page</Link></p>
+        <main className="h-screen w-screen flex-col flex justify-center text-slate-600 items-center">
+            <h2 className="font-extrabold mb-8 text-[30px] ">There was a problem</h2>
+            <p className="mb-4 text-[18px]">We could not find the page you were looking for</p>
+            <p className="text-[18px]">Go back to the <Link className="underline text-blue-500" href="/">Login page</Link></p>
         </main>
     )
 }
