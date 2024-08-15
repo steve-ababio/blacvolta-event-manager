bf8ad49540666fec47be21bf6f67732e3c2a30bd
steve-ababio <stevo12424@live.com>
2024-08-15 17:00:39 +0000

    added search feature

--- app/api/unapprovedevents/route.ts
+++ app/api/unapprovedevents/route.ts
@@ -1,8 +1,8 @@
 import {  IEvent } from "@/app/constants/constants";
 import { prisma } from "@/app/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(){
+export async function GET(req:NextRequest,res:NextResponse){
     let results:IEvent[];
     try{
          results = await prisma.event.findMany({
@@ -14,5 +14,6 @@ export async function GET(){
     }catch(error){
         return NextResponse.json({message:"Internal server error"});
     }
+    res.headers.set("Cache-Control", "no-store");
     return NextResponse.json(results)
 }
