26814476ac4c82f0c2024f5f2fa05316474ddfae
steve-ababio <stevo12424@live.com>
2024-08-27 00:52:09 +0000

    changed payment link

--- app/api/approveevent/route.ts
+++ app/api/approveevent/route.ts
@@ -20,7 +20,7 @@ export async function PUT(req:NextRequest){
             email,
             link:"https://paystack.com/pay/blacvolta",
             subject: "Your event has been Approved!",
-            text: `Follow the link below to complete payment with the event id: ${eventId} and have your event published.`
+            text: `Follow the link below to complete payment with the Event Id: ${eventId} and have your event published.`
         }
         const response = await fetch("https://mail.blacvolta.com/send.php",{body:JSON.stringify(body),method:"POST",headers:{"Content-Type":"application/json"}});
         const data = await response.json();
